03d9709399e60da1027891e2f50368c8aad1ae3a
Lightech <lightech@outlook.com>
2024-03-10 11:05:45 +0700

    SDCARD | Modify code to read a default file Hello.txt, add card formating tip

--- F411CEU6_SDCARD_SPI/Core/Src/main.c
+++ F411CEU6_SDCARD_SPI/Core/Src/main.c
@@ -44,7 +44,14 @@
 SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
+FATFS       FatFs;                //Fatfs handle
+FIL         fil;                  //File handle
+FRESULT     fres;                 //Result after operations
+char        buf[100];
 
+FATFS *pfs;
+DWORD fre_clust;
+uint32_t totalSpace, freeSpace;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -59,77 +66,37 @@ static void MX_SPI1_Init(void);
 /* USER CODE BEGIN 0 */
 void process_SD_card( void )
 {
-  FATFS       FatFs;                //Fatfs handle
-  FIL         fil;                  //File handle
-  FRESULT     fres;                 //Result after operations
-  char        buf[100];
-
   do
   {
     //Mount the SD Card
     fres = f_mount(&FatFs, "", 1);    //1=mount now
     if (fres != FR_OK)
     {
-      printf("No SD Card found : (%i)\r\n", fres);
       break;
     }
-    printf("SD Card Mounted Successfully!!!\r\n");
 
     //Read the SD Card Total size and Free Size
-    FATFS *pfs;
-    DWORD fre_clust;
-    uint32_t totalSpace, freeSpace;
-
     f_getfree("", &fre_clust, &pfs);
     totalSpace = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
     freeSpace = (uint32_t)(fre_clust * pfs->csize * 0.5);
 
-    printf("TotalSpace : %lu bytes, FreeSpace = %lu bytes\n", totalSpace, freeSpace);
-
     //Open the file
-    fres = f_open(&fil, "EmbeTronicX.txt", FA_WRITE | FA_READ | FA_CREATE_ALWAYS);
+    fres = f_open(&fil, "Hello.txt", FA_READ);
     if(fres != FR_OK)
     {
-      printf("File creation/open Error : (%i)\r\n", fres);
-      break;
-    }
-
-    printf("Writing data!!!\r\n");
-    //write the data
-    f_puts("Welcome to EmbeTronicX", &fil);
-
-    //close your file
-    f_close(&fil);
-
-    //Open the file
-    fres = f_open(&fil, "EmbeTronicX.txt", FA_READ);
-    if(fres != FR_OK)
-    {
-      printf("File opening Error : (%i)\r\n", fres);
       break;
     }
 
     //read the data
     f_gets(buf, sizeof(buf), &fil);
 
-    printf("Read Data : %s\n", buf);
-
     //close your file
     f_close(&fil);
-    printf("Closing File!!!\r\n");
-#if 0
-    //Delete the file.
-    fres = f_unlink(EmbeTronicX.txt);
-    if (fres != FR_OK)
-    {
-      printf("Cannot able to delete the file\n");
-    }
-#endif
-  } while( false );
+
+  } while( 0 );
 
   //We're done, so de-mount the drive
   f_mount(NULL, "", 0);
-  printf("SD Card Unmounted Successfully!!!\r\n");
 }
 /* USER CODE END 0 */
 
