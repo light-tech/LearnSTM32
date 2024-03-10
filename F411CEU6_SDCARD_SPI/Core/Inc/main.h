294d8a0cbfb6081c2e421bfcac6bb483dc34cb1c
Lightech <lightech@outlook.com>
2024-03-10 09:43:31 +0700

    SDCARD | Configure the project: enable SPI and FATFS middleware

--- F411CEU6_SDCARD_SPI/Core/Inc/main.h
+++ F411CEU6_SDCARD_SPI/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define SDCARD_CS_Pin GPIO_PIN_0
+#define SDCARD_CS_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
